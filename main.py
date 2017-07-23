3fbe59f05b2c41a66f8a888a80a34fef10891e83
Greg Silverman <greg.silverman@memeo-inc.com>
2017-07-23 10:58:50 -0700

    Code inspection.

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-import sys
 import array
 import string
 
+
 class HashHist(object):
     SYMBOL_COUNT = 100
+
     def __init__(self, p_word):
-        int_hist = array.array('b',[0 for i in xrange(HashHist.SYMBOL_COUNT)])
+        int_hist = array.array('b', [0 for i in xrange(HashHist.SYMBOL_COUNT)])
         self.word = string.lower(p_word)
         for c in p_word:
-            i = ord(c)-ord('a')
+            i = ord(c) - ord('a')
             int_hist[i] += 1
-        chr_hist =[' ' for i in xrange(HashHist.SYMBOL_COUNT)]
+        chr_hist = [' ' for i in xrange(HashHist.SYMBOL_COUNT)]
         for i in xrange(HashHist.SYMBOL_COUNT):
-            j = chr(ord('a')+int_hist[i])
+            j = chr(ord('a') + int_hist[i])
             chr_hist[i] = j
         self.encoding = ''.join(chr_hist)
         self.hash = hash(self.encoding)
+
     def __repr__(self):
-        s = '(%s\t,%s,% 010x)' % (self.word,self.encoding,self.hash)
+        s = '(%s\t,%s,% 010x)' % (self.word, self.encoding, self.hash)
         return s
 
-words = open('english language dictionary.txt','r')
+
+words = open('english language dictionary.txt', 'r')
 anagrams = {}
-maxanagarams = []
 counts = {}
 
 for i in words:
@@ -30,9 +32,6 @@ for i in words:
     hh = HashHist(i)
     if hh.hash in anagrams:
         anagrams[hh.hash].append(hh.word)
-        l = anagrams[hh.hash]
-        if len(l) > len(maxanagarams):
-            maxanagarams = l
     else:
         anagrams[hh.hash] = [hh.word]
 
@@ -45,12 +44,9 @@ for h in anagrams:
         counts[l] = set([h])
 
 c = counts.keys()
-c = sorted(c[:],reverse=True)
+c = sorted(c[:], reverse=True)
 for i in c:
     hs = counts[i]
     for j in hs:
         a = anagrams[j]
-        print len(a),a
-
-
-
+        print len(a), a
